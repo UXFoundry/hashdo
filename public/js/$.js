39cc13a2d93b928459ba2042fd898dc8d795ef4d
Werner van Deventer <werner@brutaldev.com>
2015-10-02 10:15:24 +0200

    Use base URL for pulling script in from the client.

--- public/js/$.js
+++ public/js/$.js
@@ -1,7 +1,7 @@
-/* global jQuery, Zepto, card */
+/* global jQuery, Zepto, card, baseUrl */
 
 if (typeof jQuery === 'undefined' || typeof Zepto === 'undefined') {
-  card.require('http://hashdo.com/js/zepto.min.js', function () {
+  card.require(baseUrl + '/js/zepto.min.js', function () {
     card.onReady && card.onReady();
   });
 }
