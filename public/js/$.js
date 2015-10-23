14c4962cd860cf35c81dbc38da23cd795f287be6
Shaun Trennery <shaun.trennery@gmail.com>
2015-10-23 11:29:50 +0200

    Add Zepto the hashdo.com CDN

--- public/js/$.js
+++ public/js/$.js
@@ -1,7 +1,7 @@
 /* global jQuery, Zepto, card, baseUrl */
 
 if (typeof jQuery === 'undefined' || typeof Zepto === 'undefined') {
-  card.require(baseUrl + '/js/zepto.min.js', function () {
+  card.require('https://cdn.hashdo.com/js/zepto.min.js', function () {
     card.onReady && card.onReady();
   });
 }
